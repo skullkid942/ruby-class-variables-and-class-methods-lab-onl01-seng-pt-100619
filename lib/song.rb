4bb9741e41b0985cc9cab0e92dc822f3e50210e0
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-29 16:09:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,12 +40,12 @@ class Song
   def artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
+      if artists[artist]
+        artists[artist] += 1
       else
-        artist_count[artist] = 1
+        artists[artist] = 1
       end
     end
-    artist_count
+    artists
   end
 end 
