b9068513787cc1688d225365773c6d699db88f57
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-29 16:05:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,16 @@ class Song
   end 
   
   def genre_count
-   
+   genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre] #if the genre is already in the hash
+        genre_count[genre] += 1
+      else #if it's a new genre
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
   
   def artist_count
     artist_count = {}
