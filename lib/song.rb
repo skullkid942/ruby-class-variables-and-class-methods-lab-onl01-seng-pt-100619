1d9edcb0ca4f6c0e8bdfdfccc7670fc0e1452cb4
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-29 16:06:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,5 +38,14 @@ class Song
   end
   
   def artist_count
-    
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] #if the artist is already in the hash
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end 
