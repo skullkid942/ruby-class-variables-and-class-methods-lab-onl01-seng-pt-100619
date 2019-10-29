bbd79e9525c6a0a4f98a263b054cbeee851e0550
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-29 15:44:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,30 @@
 class Song
+  @@count = 0 
   
   def initialize
     attr_accessor :name :artist :genre 
+    attr_writer :name :artist :genre 
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end 
+  
+  def count
+    @@count
+  end
+  
+  def genres
+    @@genres = []
+  end 
+  
+  def artists
+    @@artists = []
+  end 
+  
+  def genre_count
+    
+  end 
+  
+  def artist_count
   end 
 end 
