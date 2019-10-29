3dd6f343543949fee74b377191151098578b3d9f
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-29 16:07:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   attr_accessor :name, :artist, :genre 
-  count = 0 
+  @@count = 0 
   @@genres = []
   @@artists = []
   
@@ -40,7 +40,7 @@ class Song
   def artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist] #if the artist is already in the hash
+      if artist_count[artist]
         artist_count[artist] += 1
       else
         artist_count[artist] = 1
